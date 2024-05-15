c8555e0e81ebef4335013f6509e4100477e87bb1
emman01-ship <63071898+emman01-ship@users.noreply.github.com>
2024-05-15 00:50:18 -0400

    Frontend part3 (#5)
    * added functionality for deleting an expense in the database
    
    * model form can now make api call to back end to delete an expense along with closing out the form

---
+++ src/main/js/controller/mongo.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import axios from 'axios';
+
+export async function deleteExpense(id){
+    console.log(id);
+   await axios.delete(`http://localhost:8080/${id}`)
+.then(response => {
+console.log(`Deleted expense with ${id}`);
+})
+.catch(error => {
+console.error(error);
+});
+
+}
+
+export async function addExpense(name, category, amount){
+
+    await axios.post(`http://localhost:8080/add`, 
+    {
+        expenseName: name,
+        expenseCategory: category,
+        expenseAmount: amount
+
+    }
+    )
+    .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+}
